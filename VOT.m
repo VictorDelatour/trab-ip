ed5d9714fb97aec29e5a980961b5ae19fb7b23c0
Hugo Babel <hugo.babel@epfl.ch>
2015-10-29 16:55:08 +0100

    Corrected the VOT implementation, but the result is extremely unefficient.

--- VOT.m
+++ VOT.m
@@ -7,113 +7,100 @@ r2 = radius^2;
 n_angle = 10;
 f_angle = n_angle/360;
 
-[I, J] = ind2sub([nrow, ncol], find(~isnan(image)));
+ind = find(~isnan(image));
 
+[I, J] = ind2sub([nrow, ncol], ind);
 
-data = cell(n_angle, radius);
+data = cell(n_angle, 2*radius);
 nCells = numel(data);
-index = ones(n_angle, radius);
+index = ones(n_angle, 2*radius);
 
 % init_cell
 for i = 1:nCells
-   data{i} = -ones(1,1); 
+    data{i} = -ones(1,1);
 end
 
-X = repmat(1:ncol, nrow, 1);
-Y = repmat((1:nrow)', 1, ncol);
 %% Compute matrix
 
 for idx = 1:numel(I)
     i = I(idx); j = J(idx);
+    fprintf('%i of %i\n', idx, numel(I));
     
-    ind = find((X-i).^2+(Y-j).^2<r2 &  ~(X==i & Y==j) & ~isnan(image)); % Use logical indexing instead?
+    loc_ind = ind( (I-i).^2+(J-j).^2 < r2 & ~(I==i & J == j));
+    [loc_I, loc_J] = ind2sub( size(image), loc_ind);
     
-    dist = round(sqrt((X(ind)-i).^2+(Y(ind)-j).^2));
-    angle = round(f_angle*(atan2d(Y(ind)-j, X(ind)-i)+180))+1; % !!! Check the angle!
-    
-    angle(angle>n_angle) = 1; % Set 0 \equiv 360,
-    
-    for p = 1:numel(ind)
+    for loc_idx = 1:numel(loc_ind)
         
-        ap = angle(p); % !!! Check the angle!
-        dp = dist(p);
+        loc_i = loc_I(loc_idx); loc_j = loc_J(loc_idx);
+        loc_diff = abs(image(loc_i, loc_j)-image(loc_ind)); % Get gray value difference
+        loc_dist = sqrt( (loc_I - loc_i).^2 + (loc_J - loc_j).^2);
+        loc_ang = round(f_angle*(atan2d(loc_J-j, loc_I-i)+270))+1;
+        loc_ang(loc_ang > n_angle) = 1;
         
-        % If no more storage, expand vector
-        if numel(data{ap, dp}) < index(ap, dp) + 1
-            temp = data{ap, dp};
-            data{ap, dp} = -ones(numel(temp)+10,1);
-            data{ap, dp}(1:numel(temp))=temp;
+        sub_ind = loc_dist > 0 & loc_J >= loc_j & (loc_J ~= loc_j | loc_I >= loc_i);
+        
+        loc_ang = loc_ang(sub_ind);
+        loc_diff = loc_diff(sub_ind);
+        loc_dist = round(loc_dist(sub_ind));
+        
+        for k = 1:numel(loc_diff)
+            ap = loc_ang(k);
+            dp = loc_dist(k);
+            if numel(data{ap, dp}) < index(ap, dp)+1
+                temp = data{ap, dp};
+                data{ap, dp} = -ones(numel(temp)+10,1);
+                data{ap, dp}(1:numel(temp))=temp;
+            end
+            data{ap, dp}(index(ap, dp)) = loc_diff(k);
+            index(ap, dp) = index(ap, dp) + 1;
         end
         
-        data{ap, dp}(index(ap, dp)) = abs(image(Y(ind(p)), X(ind(p)))- image(i,j));
-        
-        index(ap, dp) = index(ap, dp) + 1;
-        
     end
-    
 end
         
 
 %% Compute variance and Hurst coefficients
 
-variance = zeros(n_angle, radius);
-max_diff = zeros(n_angle, radius);
+variance = zeros(n_angle, 2*radius);
 
-n_scale = radius-4;
+n_scale = 2*radius-4;
 
-v_dist = 1:radius;
+v_dist = 1:2*radius;
 v_ang = ((1:n_angle)./f_angle)./180*pi;
 
 hurst_var = zeros(n_angle, n_scale);
-% hurst_dif = zeros(n_angle, n_scale);
-hurst_dif = zeros(n_angle,1);
 
-im_sd = std(image(~isnan(image)));
+
+
 
 %%
 for ap = 1:n_angle
     
     % Compute variance for each angular "slice"
-    for dp = 1:radius
+    for dp = 1:2*radius
         data{ap, dp} = data{ap, dp}(data{ap, dp}~=-1);
         if numel(data{ap, dp})~=0
             variance(ap, dp) = var(data{ap, dp});
-            max_diff(ap, dp) = max(data{ap, dp});
         else
             variance(ap, dp) = NaN;
-            max_diff(ap, dp) = NaN;
         end
     end
-    
-    % Original HOT method divides by sd of all pixel brightness values
-    max_diff(ap,:) = max_diff(ap,:) / im_sd;
-    
     
     % Get coef by linear regression for each loglog data
     hurst_var(ap, :) = get_hurst_coef(v_dist, variance(ap,:), 'VOT');
     
-
-    % HOT coef are computed by fitting a linear regression for every angle
-    ind = find(~isnan(max_diff(ap,:)));
-    pfit = polyfit(log(v_dist(ind)), log(max_diff(ap,ind)), 1);
-    pval = polyval(pfit, log(v_dist(ind)));
-    
-    figure(1);
-    plot(log(v_dist(ind)), log(max_diff(ap,ind)), 'o', log(v_dist(ind)), pval, '-r' );
-    waitforbuttonpress;
-    
     % Compute R^2 for linear regression
-    resid = log(max_diff(ap, ind))-pval;
-    SSresid = sum(resid.^2);
-    SStotal = (numel(resid)-1) * var(log(max_diff(ap, ind)));
-    rsq = 1- SSresid/SStotal;
-    rsq_adj = 1-SSresid/SStotal * (numel(resid)-1)/(numel(resid)-numel(pfit));
+%     resid = log(max_diff(ap, ind))-pval;
+%     SSresid = sum(resid.^2);
+%     SStotal = (numel(resid)-1) * var(log(max_diff(ap, ind)));
+%     rsq = 1- SSresid/SStotal;
+%     rsq_adj = 1-SSresid/SStotal * (numel(resid)-1)/(numel(resid)-numel(pfit));
     
-    if rsq_adj > .95
-        hurst_dif(ap) = pfit(1);
-    else
-        hurst_dif(ap) = NaN;
-    end
+%     if rsq_adj > .95
+%         hurst_dif(ap) = pfit(1);
+%     else
+%         hurst_dif(ap) = NaN;
+%     end
     
     
 end
@@ -122,6 +109,5 @@ end
 
 [FD_VOT, S_tr_VOT, S_td_VOT] = get_OT_vars(hurst_var, 'VOT');
 
-[FD_HOT, S_tr_HOT, S_td_HOT] = get_OT_vars(hurst_dif, 'HOT');
 
 end
